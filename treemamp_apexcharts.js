2293b8f810b3cb3e74c7d7976c601dbeaadd8532
Yadder Aceituno <yadderace@gmail.com>
2023-03-16 17:01:34 -0600

    Testing for fixing error

--- treemamp_apexcharts.js
+++ treemamp_apexcharts.js
@@ -1,89 +1,4 @@
 
-looker.plugins.visualizations.add({
-    // Id and Label are legacy properties that no longer have any function besides documenting
-    // what the visualization used to have. The properties are now set via the manifest
-    // form within the admin/visualizations page of Looker
-    id: "treemap_apexcharts",
-    label: "Treemap Apexcharts",
-    
-    options: create_fixed_options(),
-
-
-    // Set up the initial state of the visualization
-    create: function(element, config) {
-  
-        create_div(element);
-  
-    },
-    // Render in response to the data or settings changing
-    updateAsync: function(data, element, config, queryResponse, details, done) {
-  
-      // Clear any errors from previous updates
-      this.clearErrors();
-      create_div(element);
-
-      var options = create_options(queryResponse);
-
-      // Validating fields
-      if(queryResponse.fields.dimensions.length != 2){
-        this.addError({
-          title: "Two Dimensions Required",
-          message: "This visualization requires two dimensions"
-        });
-        return;
-      } else if(queryResponse.fields.measures.length != 1){
-        this.addError({
-          title: "One Measure Required",
-          message: "This visualization requires one measure"
-        });
-        return;
-      }
-
-      // Creating options from data
-      var options = create_options(queryResponse);
-      this.trigger('registerOptions', options);
-      
-      // Transforming data
-      const measure_name = queryResponse.fields.measures[0].name;
-      const dimension_color = queryResponse.fields.dimensions[0].name;
-      const dimension_label = queryResponse.fields.dimensions[1].name;
-      var series_apexchart = transform_data_to_treemap(data, measure_name, dimension_color, dimension_label);
-
-
-      // Setting data and configuration for the chart
-      var options = {
-        
-        series: series_apexchart,
-
-        colors: config.color_range,
-        
-        legend: {
-          show: config.show_legend
-        },
-        
-        chart: {
-          height: 350,
-          type: 'treemap',
-          toolbar: {
-            show: false
-          }
-        },
-
-        plotOptions: {
-          treemap:{
-            useFillColorAsStroke: config.fill_color_as_stroke
-          }
-        }
-      };
-
-      // Rendering the chart
-      var chart = new ApexCharts(element.querySelector("#chart"), options);
-      chart.render();
-      done();
-    }
-  }
-);
-
 const DIV_ID = 'chart';
 
 const transform_data_to_treemap = function (data, measure_name, dimension_color, dimension_label){
@@ -171,3 +86,89 @@ const create_dynamic_options = function(queryResponse){
   
   return options;
 }
+
+
+looker.plugins.visualizations.add({
+    // Id and Label are legacy properties that no longer have any function besides documenting
+    // what the visualization used to have. The properties are now set via the manifest
+    // form within the admin/visualizations page of Looker
+    id: "treemap_apexcharts",
+    label: "Treemap Apexcharts",
+    
+    options: create_fixed_options(),
+
+
+    // Set up the initial state of the visualization
+    create: function(element, config) {
+  
+        create_div(element);
+  
+    },
+    // Render in response to the data or settings changing
+    updateAsync: function(data, element, config, queryResponse, details, done) {
+  
+      // Clear any errors from previous updates
+      this.clearErrors();
+      create_div(element);
+
+      var options = create_options(queryResponse);
+
+      // Validating fields
+      if(queryResponse.fields.dimensions.length != 2){
+        this.addError({
+          title: "Two Dimensions Required",
+          message: "This visualization requires two dimensions"
+        });
+        return;
+      } else if(queryResponse.fields.measures.length != 1){
+        this.addError({
+          title: "One Measure Required",
+          message: "This visualization requires one measure"
+        });
+        return;
+      }
+
+      // Creating options from data
+      var options = create_options(queryResponse);
+      this.trigger('registerOptions', options);
+      
+      // Transforming data
+      const measure_name = queryResponse.fields.measures[0].name;
+      const dimension_color = queryResponse.fields.dimensions[0].name;
+      const dimension_label = queryResponse.fields.dimensions[1].name;
+      var series_apexchart = transform_data_to_treemap(data, measure_name, dimension_color, dimension_label);
+
+
+      // Setting data and configuration for the chart
+      var options = {
+        
+        series: series_apexchart,
+
+        colors: config.color_range,
+        
+        legend: {
+          show: config.show_legend
+        },
+        
+        chart: {
+          height: 350,
+          type: 'treemap',
+          toolbar: {
+            show: false
+          }
+        },
+
+        plotOptions: {
+          treemap:{
+            useFillColorAsStroke: config.fill_color_as_stroke
+          }
+        }
+      };
+
+      // Rendering the chart
+      var chart = new ApexCharts(element.querySelector("#chart"), options);
+      chart.render();
+      done();
+    }
+  }
+);
